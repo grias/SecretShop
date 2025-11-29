5057e0e61a2437708251dd661ea69c4d29562d30
Alexander Gribowski <grias1994@gmail.com>
2025-11-29 21:23:14 +0300

    Changed UserDto to include email and role

--- UsersManagementService/Application/Dtos/Responses/UserDto.cs
+++ UsersManagementService/Application/Dtos/Responses/UserDto.cs
@@ -1,7 +1,11 @@
-﻿namespace InnoShop.UsersManagementService.Application.Dtos.Responses;
+﻿using InnoShop.UsersManagementService.Domain.Enumerators;
+
+namespace InnoShop.UsersManagementService.Application.Dtos.Responses;
 
 public class UserDto
 {
     public int Id { get; set; }
     public string Username { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public UserRoles Role { get; set; } = UserRoles.User;
 }
